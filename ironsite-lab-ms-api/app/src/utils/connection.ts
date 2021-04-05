d44bd66f14e8b139e9f5d72c4bcbe9b7a939a913
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2021-04-04 22:58:55 -0500

    structure project

--- ironsite-lab-ms-api/app/src/utils/connection.ts
+++ ironsite-lab-ms-api/app/src/utils/connection.ts
@@ -9,7 +9,7 @@ export interface PromisifiedPool extends Omit<Pool, "query"> {
 const params = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 };
 
