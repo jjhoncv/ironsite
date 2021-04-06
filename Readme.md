b0c2f0ed790fc5e047de07a1f27305df092b2116
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2021-04-05 23:30:12 -0500

    structure project

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# ironsite-lab-ms-api-bd
+- docker
+- nodejs
+- express
+- mysql
+
+## local
+
+```sh
+make install
+```
+
+```sh
+make start
+```
+
+```sh
+make stop
+```
+## production
+
+```sh
+make build
+```
+
+```sh
+make deploy
+```
+
+<!-- # install node
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node -->
---
+++ ironsite-lab-ms-api/makefiles/deploy.ec2.mk
@@ -0,0 +1,22 @@
+SERVER_NAME     = ubuntu@ec2-54-95-81-0.ap-northeast-1.compute.amazonaws.com
+PROJECT_SERVER ?= ${SERVER_NAME}:~/${PROJECT_NAME}
+
+build:
+	@docker run \
+		-it \
+		--rm \
+		-u 1000:1000 \
+		--name ${PROJECT_NAME} \
+		-v $(PWD)/app:/app \
+		-w /app \
+		node:11-slim \
+		npm run build
+	@cp -r app/package.json app/dist
+	@cp -r scripts app/dist
+	@cp app/.env.prod app/dist/.env
+
+deploy:
+	@rsync -avL --del -e "ssh -i DashboardApiKey.pem" \
+		app/dist/ ${PROJECT_SERVER}
+	ssh -i DashboardApiKey.pem ${SERVER_NAME} \
+	'cd ~/${PROJECT_NAME}; sh scripts/start.sh ${PROJECT_NAME}'
